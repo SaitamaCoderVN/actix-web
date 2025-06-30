0603ecdd91e2cbffc9d1e7863a0e989ed0272e1f
SaitamaCoderVN <coderhopham@gmail.com>
2025-06-30 19:54:11 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok(); // Load .env file
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    
+    println!("Server starting on port {}", port);
+    
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+    .bind(format!("127.0.0.1:{}", port))?
+    .run()
+    .await
+}
